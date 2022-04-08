687883d780dda262567f0e6322f54b577a45e3aa
tahmidradit <tahmid.radit@outlook.com>
2022-04-08 12:29:53 +0600

    Category Controller added

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crisp.Controllers
+{
+    [Route("api/[Controller]/[action]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        // public IActionResult Index()
+        // {
+        //     return View();
+        // }
+    }
+}
