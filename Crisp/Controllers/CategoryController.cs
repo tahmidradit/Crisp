0ef254d6104488f31721a3501bd727146abee5bd
tahmidradit <tahmid.radit@outlook.com>
2022-04-26 17:07:14 +0600

    Category API completed

--- Crisp/Controllers/CategoryController.cs
+++ Crisp/Controllers/CategoryController.cs
@@ -9,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Crisp.Controllers
 {
+#nullable disable
     [Route("api/[Controller]")]
     [ApiController]
     public class CategoryController : ControllerBase
@@ -17,20 +18,66 @@ namespace Crisp.Controllers
         public CategoryController(ApplicationDbContext context)
         {
             this.context = context;
-
         }
 
-        // [HttpGet]
-        // public async Task<IActionResult> GetAllCategories()
-        // {
-        //     var getAllCategories = await context.Categories.ToListAsync();
-        //     return Ok(getAllCategories);
-        // }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var getAllCategories = await context.Categories.ToListAsync();
+            return Ok(getAllCategories);
+        }
 
-        // [HttpPost]
-        // public async Task<IActionResult> CreateCategory([FromBody] Category category)
-        // {
-        //     category.Id = Guid.NewGuid();
-        // }
+        [HttpGet]
+        [Route("{id:guid}")]
+        [ActionName("GetSingleCategory")]
+        public async Task<IActionResult> GetSingleCategory([FromRoute] Guid id)
+        {
+            var getSingleCategory = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (getSingleCategory != null)
+            {
+                return Ok(getSingleCategory);
+            }
+            return NotFound("Category not found !");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddCategory([FromBody] Category category)
+        {
+            category.Id = Guid.NewGuid();
+            await context.Categories.AddAsync(category);
+            await context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetSingleCategory), new { id = category.Id }, category);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> UpdateCategory([FromRoute] Guid id, Category category)
+        {
+            var getSingleCategory = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (getSingleCategory != null)
+            {
+                getSingleCategory.Name = category.Name;
+                await context.SaveChangesAsync();
+                return Ok(getSingleCategory);
+            }
+            return NotFound("Category not found !");
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public async Task<IActionResult> DeleteCategory([FromRoute] Guid id)
+        {
+            var getSingleCategory = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (getSingleCategory != null)
+            {
+                context.Categories.Remove(getSingleCategory);
+                await context.SaveChangesAsync();
+                return Ok(getSingleCategory);
+            }
+            return NotFound("Category not found !");
+        }
     }
 }
