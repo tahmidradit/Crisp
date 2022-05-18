329d7a0d319712c4196d093ea975a633aa314577
tahmidradit <tahmid.radit@outlook.com>
2022-05-18 12:00:30 +0600

    Service binded to getCategory component

--- Crisp/ClientApp/Crisp/src/app/category/get-category/get-category.component.ts
+++ Crisp/ClientApp/Crisp/src/app/category/get-category/get-category.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CategoryService } from 'src/app/services/category/category.service';
 
 @Component({
   selector: 'app-get-category',
   templateUrl: './get-category.component.html',
-  styleUrls: ['./get-category.component.css']
+  styleUrls: ['./get-category.component.css'],
 })
 export class GetCategoryComponent implements OnInit {
+  getCategories$!: Observable<any[]>;
 
-  constructor() { }
+  constructor(private service: CategoryService) {}
 
   ngOnInit(): void {
+    this.getCategories$ = this.service.getCategories();
   }
-
 }
