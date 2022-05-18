ee257a4cc39b0ae3462b88e91ca2aa9ddcf4cf20
tahmidradit <tahmid.radit@outlook.com>
2022-05-18 10:46:26 +0600

    Category get method implemented and caches modified

--- Crisp/ClientApp/Crisp/src/app/services/category/category.service.ts
+++ Crisp/ClientApp/Crisp/src/app/services/category/category.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CategoryService {
+  readonly baseUrl = 'https://localhost:7101/api';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getCategory(): Observable<any[]> {
+    return this.http.get<any>(this.baseUrl + '/category');
+  }
 }
