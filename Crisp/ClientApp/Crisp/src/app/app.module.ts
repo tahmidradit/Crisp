bfc095f2aaebb141e18ce8bf1178ac829309fa62
tahmidradit <tahmid.radit@outlook.com>
2022-05-29 21:33:36 +0600

    Category table moved into Category Index page

--- Crisp/ClientApp/Crisp/src/app/app.module.ts
+++ Crisp/ClientApp/Crisp/src/app/app.module.ts
@@ -5,13 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GetCategoryComponent } from './category/get-category/get-category.component';
 import { CategoryService } from 'src/app/services/category/category.service';
 import { AddCategoryComponent } from './category/add-category/add-category.component';
 import { CategoryIndexComponent } from './category/category-index/category-index.component';
 
 @NgModule({
-  declarations: [AppComponent, GetCategoryComponent, AddCategoryComponent, CategoryIndexComponent],
+  declarations: [AppComponent, AddCategoryComponent, CategoryIndexComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
