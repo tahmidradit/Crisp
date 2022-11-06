4e2a6cea138ece2ece18d73de172954ec3ac6f30
tahmidradit <tahmid.radit@outlook.com>
2022-11-06 23:02:32 +0600

    Added card service, component

--- Crisp/ClientApp/Crisp/src/app/app.module.ts
+++ Crisp/ClientApp/Crisp/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CardService } from './services/card/card.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -9,23 +10,18 @@ import { CategoryService } from 'src/app/services/category/category.service';
 import { AddCategoryComponent } from './category/add-category/add-category.component';
 import { CategoryIndexComponent } from './category/category-index/category-index.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { AddCardComponent } from './card/add-card/add-card.component';
 
 @NgModule({
-  declarations: [AppComponent, AddCategoryComponent, CategoryIndexComponent, HomepageComponent],
+  declarations: [AppComponent, AddCategoryComponent, CategoryIndexComponent, HomepageComponent, AddCardComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule,
-    ToastrModule.forRoot(),
-    BrowserAnimationsModule,
-    NgxPaginationModule
+    ReactiveFormsModule
   ],
-  providers: [CategoryService],
+  providers: [CategoryService, CardService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- Crisp/ClientApp/Crisp/src/app/services/category/category.service.ts
+++ Crisp/ClientApp/Crisp/src/app/services/category/category.service.ts
@@ -6,6 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class CategoryService {
+  
   readonly categoryBaseUrl = 'https://localhost:7101/api/category';
 
   constructor(private http: HttpClient) {}
